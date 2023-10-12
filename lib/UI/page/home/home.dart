931df085f56953faa78810a3d3cecf64e2b73956
taengK <ktk0646@gmail.com>
2023-10-12 11:14:51 +0900

    c

---
+++ lib/UI/page/home/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:project_alfa/UI/widgets/home/TopNavigationBar.dart';
+
+class Home extends StatelessWidget {
+  Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: HomeBody(),
+    );
+  }
+}
+
+HomeBody() => SafeArea(
+        child: Column(
+      children: <Widget>[TopNavigationBar()],
+    ));
