e3db83f719dd1df6ce2e5e6d21ab613580be829f
Pumpkin980423 <tngh5352@gmail.com>
2023-10-13 11:23:15 +0900

    4차 커밋

--- project_alfa/lib/UI/page/home/home.dart
+++ project_alfa/lib/UI/page/home/home.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:project_alfa/UI/widgets/home/Footer.dart';
+import 'package:project_alfa/UI/widgets/home/Home_first.dart';
 import 'package:project_alfa/UI/widgets/home/TopNavigationBar.dart';
 
-class Home extends StatelessWidget {
-  Home({super.key});
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,97 +26,21 @@ HomeBody() => SafeArea(
         Expanded(
             child: SingleChildScrollView(
           child: Column(
-            children: <Widget>[
-              First(),
-            ],
+            children: <Widget>[Home_first(), Second(), Footer()],
           ),
         ))
       ],
     ));
 
-class First extends StatelessWidget {
-  const First({super.key});
+class Second extends StatelessWidget {
+  const Second({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       height: 900,
-      child: Column(
-        children: <Widget>[
-          SizedBox(
-            height: 50.0,
-          ),
-          SizedBox(
-            width: 1148,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text.rich(TextSpan(children: <TextSpan>[
-                  TextSpan(
-                    text: "ALLOY",
-                    style: TextStyle(
-                      fontSize: 80,
-                      color: Color.fromRGBO(182, 24, 24, 1),
-                    ),
-                  ),
-                  TextSpan(
-                    text:
-                        "\t\talloyalloyalloyalloyalloy\nalloyalloyalloyalloyalloy\nalloyalloyalloyalloyalloyalloyalloy",
-                    style: TextStyle(
-                      fontSize: 30,
-                    ),
-                  )
-                ])),
-                Column(
-                  children: [
-                    SizedBox(
-                      height: 140,
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/main');
-                      },
-                      child: Text(
-                        "GET STARTED",
-                        style: TextStyle(color: Colors.white, fontSize: 20),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                          elevation: 5,
-                          backgroundColor: Colors.black,
-                          padding: EdgeInsets.fromLTRB(35, 20, 35, 20),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10))),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Image.asset('assets/image/image9.png'),
-          SizedBox(
-            height: 70,
-          ),
-          Text(
-              "Alone in the night  On a dark hill With pines around me  Spicy and still,"),
-          SizedBox(
-            height: 10,
-          ),
-          Container(
-            height: 1.0,
-            width: 1148.0,
-            color: const Color.fromRGBO(217, 217, 217, 100),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Text(
-              "And a heaven full of stars Over my head, White and topaz And misty red Myriads with beating Hearts of fire That aeons")
-        ],
-      ),
+      color: Colors.red,
     );
   }
 }
---
+++ project_alfa/lib/UI/widgets/home/Home_first.dart
@@ -0,0 +1,157 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+class Home_first extends StatefulWidget {
+  const Home_first({Key? key}) : super(key: key);
+
+  @override
+  State<Home_first> createState() => _Home_firstState();
+}
+
+int _current = 0;
+
+final CarouselController _controller = CarouselController();
+
+List imageList = ["assets/image/image9.png", "assets/image/first_img.jpg"];
+
+class _Home_firstState extends State<Home_first> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 900.0,
+      child: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 50.0,
+          ),
+          SizedBox(
+            width: 1200,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text.rich(TextSpan(children: <TextSpan>[
+                  TextSpan(
+                    text: "ALLOY",
+                    style: TextStyle(
+                      fontSize: 80,
+                      color: Color.fromRGBO(182, 24, 24, 1),
+                    ),
+                  ),
+                  TextSpan(
+                    text:
+                        "\t\talloyalloyalloyalloyalloy\nalloyalloyalloyalloyalloy\nalloyalloyalloyalloyalloyalloyalloy",
+                    style: TextStyle(
+                      fontSize: 30.0,
+                    ),
+                  )
+                ])),
+                Column(
+                  children: [
+                    SizedBox(
+                      height: 140.0,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/main');
+                      },
+                      child: Text(
+                        "GET STARTED",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                          elevation: 5,
+                          backgroundColor: Colors.black,
+                          padding: EdgeInsets.fromLTRB(35, 20, 35, 20),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10))),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          ImageSlider(),
+          SizedBox(
+            height: 50.0,
+          ),
+          Text(
+              "Alone in the night  On a dark hill With pines around me  Spicy and still,"),
+          SizedBox(
+            height: 10.0,
+          ),
+          Container(
+            height: 1.0,
+            width: 1200.0,
+            color: const Color.fromRGBO(217, 217, 217, 100),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Text(
+              "And a heaven full of stars Over my head, White and topaz And misty red Myriads with beating Hearts of fire That aeons")
+        ],
+      ),
+    );
+  }
+}
+
+class ImageSlider extends StatefulWidget {
+  const ImageSlider({super.key});
+
+  @override
+  State<ImageSlider> createState() => _ImageSliderState();
+}
+
+class _ImageSliderState extends State<ImageSlider> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: [
+      CarouselSlider(
+          carouselController: _controller,
+          items: imageList.map((imgLink) {
+            return Builder(builder: (context) {
+              return SizedBox(
+                width: 1200.0,
+                child: Image(fit: BoxFit.fill, image: AssetImage(imgLink)),
+              );
+            });
+          }).toList(),
+          options: CarouselOptions(
+            height: 500,
+            viewportFraction: 1.0,
+            autoPlay: true,
+            autoPlayInterval: const Duration(seconds: 7),
+            onPageChanged: (index, reason) {
+              setState(() {
+                _current = index;
+              });
+            },
+          )),
+      Align(
+        alignment: Alignment.bottomCenter,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: imageList.asMap().entries.map((e) {
+            return GestureDetector(
+              onTap: () => _controller.animateToPage(e.key),
+              child: Container(
+                width: 12,
+                height: 12,
+                margin:
+                    const EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.white
+                        .withOpacity(_current == e.key ? 0.9 : 0.4)),
+              ),
+            );
+          }).toList(),
+        ),
+      )
+    ]);
+  }
+}
