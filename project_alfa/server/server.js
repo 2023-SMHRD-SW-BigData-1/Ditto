0d3203ea212a83b26ad5487cf6391a0c3426e8ce
taengK <ktk0646@gmail.com>
2023-10-17 11:09:43 +0900

    express mysql2

---
+++ project_alfa/server/server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const dbConfig = require('./config/database');
+const app = express();
+const cors = require('cors');
+const http = require('http');
+
+let conn = dbConfig.init();
+
+dbConfig.connect(conn);
+
+// const connection = mysql.createConnection(dbConfig);
+
+// conn.connect((err) => {
+//   if (err) {
+//     console.error('MySQL 연결 오류: ' + err.stack);
+//     return;
+//   }
+//   console.log('MySQL에 성공적으로 연결되었습니다.');
+// });
+
+
+const server = http.createServer(app)
+app.use(cors());
+
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.set('port',process.env.PORT || 8889)
+
+
+// app.use(express.static(path.join(__dirname, '../build')))
+// app.get('/', (req, res) =>{
+//    res.sendFile(path.join(__dirname, '../build/web/index.html'))
+// })
+
+// app.get('/', (req, res) =>{
+//     res.json({
+//         "id": "testid"
+//     });
+// })
+
+app.get('/', (req, res) => {
+    conn.query('SELECT * FROM user_info', (err, rows) => {
+        if (err) {
+          console.error('쿼리 오류: ' + err);
+          return;
+        }
+        // 쿼리 결과 처리
+        console.log(rows);
+        res.send(rows);
+      });
+  });
+  
+  
+  
+
+ server.listen(app.get('port'),()=>{
+    console.log(app.get('port'),'번 포트에서 서버연결 대기중..');
+  });
